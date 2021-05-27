a300979cd267c95ac75ef156ba448cb5fef76e53
julians83 <julianmunozrua@gmail.com>
2021-05-27 02:01:06 -0500

    Maquetacion inicial

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,59 @@
 <template>
-  <div id="inicio">
-    <h1>Consultar datos</h1>
-    <button v:on:click="fetch">Consultar</button>
+  <div id="app">
+
+    <div class="hero is-white is-gradient is-bold">
+
+      <div class="hero-body">
+        <h1 class="title">
+          <span class="has-text-success">
+            Rick And Morty
+          </span>
+          <span class="subtitle">
+            Personajes
+          </span>
+
+        </h1>
+
+      </div>
+
+
+    </div>
+
+    <div class="container">
+     <div class="colunms is-desktop is-mobile is-tablet is-multiline is-centered">
+
+       <div class="colunms is-12-mobile is-4-desktop is-4-tablet" v-for="post of posts" v-bind:key="post.id">
+      <div class="card">
+        <div class="card-header">
+          <img v-bind:src="post.image" v-bind:alt="post.name">
+    
+            <h3><span>Name: </span>{{post.name}}</h3>
+            <h3><span>Status: </span>{{post.status}}</h3>
+            <h3><span>Species: </span>{{post.species}}</h3>
+            
+        </div>
+
+        
+       </div>
+
+
+     </div>
+
+
+
+
+      </div>
+
+    </div>
+
+
+
+    <!-- <h1>Consultar datos</h1>
+    <div class="post" >
+      {{ post }}
+
+    </div> -->
+ 
 
   </div>
 </template>
@@ -10,19 +62,23 @@
 import axios from 'axios'
 export default {
   dependencies: axios,
-  name:'Inicio',
+  name:'app',
   data(){
     return{
-      post: []
+      posts: []
     }
   },
   
   mounted() {
-    axios.get('https://rickandmortyapi.com/api/character')
+    let vue = this;
+    axios.get('https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20')
     .then( function( response ) {
-      console.log(response)
+        vue.posts = response.data;
     })
-  }
+  },
+  
+
+  
     
   
 }
